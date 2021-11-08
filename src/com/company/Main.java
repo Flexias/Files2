695a28de0f76663646beddb9f5a51153615ced4f
Evgeniy Kontcevoy <e.kontsevoy@foxford.ru>
2021-11-08 17:10:27 +0300

    update commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,21 +4,30 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.ObjectOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Objects;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class Main {
 
     public static void main(String[] args) {
-        File dirSave = new File("E://Games//savegames");
-        String waySave = "E://Games//savegames//save.dat";
-        String watZip = "E://Games//savegames//zip.zip";
+        ArrayList<String> waySave = new ArrayList<String>();
+        File dir = new File("E://Games//savegames");
+        waySave.add("E://Games//savegames//save1.dat");
+        waySave.add("E://Games//savegames//save2.dat");
+        waySave.add("E://Games//savegames//save3.dat");
+        String wayZip = "E://Games//savegames//zip.zip";
         GameProgress gameProgress1 = new GameProgress(60, 3, 20, 8.6);
         GameProgress gameProgress2 = new GameProgress(25, 5, 80, 35.3);
         GameProgress gameProgress3 = new GameProgress(52, 2, 35, 12.4);
-        saveGame(waySave,gameProgress1);
-        zipFiles(watZip,waySave);
-        clearFiles(dirSave);
+        saveGame(waySave.get(0),gameProgress1);
+        saveGame(waySave.get(1),gameProgress2);
+        saveGame(waySave.get(2),gameProgress3);
+        zipFiles(wayZip, waySave);
+        clearFiles(dir);
 
 
     }
@@ -32,27 +41,34 @@ public class Main {
         }
     }
 
-    public static void zipFiles(String wayZip, String waySave){
-        try (ZipOutputStream zout = new ZipOutputStream(new FileOutputStream(wayZip));
-        FileInputStream fis = new FileInputStream(waySave)) {
-            ZipEntry entry = new ZipEntry("packed_save.dat");
-            zout.putNextEntry(entry);
-            byte[] buffer = new byte[fis.available()];
-            fis.read(buffer);
-            zout.write(buffer);
-            zout.closeEntry();
-        } catch (Exception ex){
+    public static void zipFiles(String wayZip, ArrayList<String> waySave ) {
+        try (ZipOutputStream zout = new ZipOutputStream(new FileOutputStream(wayZip));) {
+            for (int i = 0; i < waySave.size(); i++) {
+                FileInputStream fis = new FileInputStream(waySave.get(i));
+                ZipEntry entry = new ZipEntry("packed_save" + i + ".dat");
+                zout.putNextEntry(entry);
+                byte[] buffer = new byte[fis.available()];
+                zout.write(buffer);
+                fis.read(buffer);
+                zout.closeEntry();
+                fis.close();
+//                Files.delete(Path.of(waySave.get(i)));
+            }
+
+        } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
     }
 
-    public static void clearFiles(File dir){
-        if (dir.isDirectory()){
-            for (File item : dir.listFiles()){
-                if (item.getName().equals("zip.zip") == false){
+
+    public static void clearFiles(File dir) {
+        if (dir.isDirectory()) {
+            for (File item : Objects.requireNonNull(dir.listFiles())) {
+                if (!item.getName().equals("zip.zip")) {
                     item.delete();
                 }
             }
         }
     }
+
 }
